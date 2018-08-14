235bd34b76e7bbb9f6c0018c185b50d1ca84f7e2
Michael Mockus <mmockus@gmail.com>
2018-08-13 20:30:44 -0500

    extract terraform data

--- example.tf
+++ example.tf
@@ -1,7 +1,17 @@
+# Comments blocks use hashes #
+
 provider "aws" {
   region     = "us-east-2"
 }
 resource "aws_instance" "example" {
   ami           = "ami-40142d25"
   instance_type = "t2.nano"
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+}
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.example.id}"
 }
